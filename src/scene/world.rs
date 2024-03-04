e74ab08e5aed635c5e34b8230bae6cd9e6fec5be
Felix Sanchez <felix.sanchez.dev@gmail.com>
2024-03-03 21:45:42 -0600

    world to scene

--- src/scene/world.rs
+++ src/scene/world.rs
@@ -1,8 +1,7 @@
-use crate::{render::{atlas::{Atlas, MaterialType}, mesh::Mesh, model::Model, pipelines::constructor as pipeline_constructor, renderer::{Draw, Renderer}, Vertex }, scene::camera::CameraLayout};
-
-use self::block::{Block, QuadVertex};
-
 pub mod block;
+use crate::render::{atlas::{Atlas, MaterialType}, mesh::Mesh, model::Model, pipelines::{constructor as pipeline_constructor, terrain::{self, TerrainPipeline}}, renderer::{Draw, Renderer}, Vertex };
+use crate::render::pipelines::GlobalsLayouts;
+use super::world::block::{Block, QuadVertex};
 
 use wgpu::Error;
 
@@ -18,29 +17,14 @@ impl World {                        ///
     pub fn new(renderer: &Renderer) -> Self {
         let atlas = Atlas::new(&renderer.device, &renderer.queue).unwrap();
 
-        let camera_layout = CameraLayout::new(&renderer.device);
-        let render_pipeline_layout = renderer.device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-            label: Some("Render Pipeline Layout"),
-            bind_group_layouts: &[&atlas.texture_bind_group_layout, &camera_layout.bind_group_layout],
-            push_constant_ranges: &[],
-        });
-
-        
-
         let shader = renderer.device.create_shader_module(wgpu::include_wgsl!("../../assets/shaders/shader.wgsl"));
 
-
-
-
-        let pipeline = pipeline_constructor(
-            &renderer.device,
-            &render_pipeline_layout,
-            &[QuadVertex::desc()],
-            wgpu::PrimitiveTopology::TriangleList,
+        let terrain_pipeline = TerrainPipeline::new(
+            &renderer.device, 
+            &GlobalsLayouts::new(&renderer.device),
             shader,
             &renderer.config
         );
-
         
         let block = Block::new(MaterialType::GRASS, [0,0,0], [0,0,0]);
 
@@ -52,7 +36,7 @@ impl World {                        ///
 
         Self {
 
-            pipeline,
+            pipeline: terrain_pipeline.pipeline,
             atlas,
             model
         }
@@ -65,9 +49,9 @@ impl Draw for World {
 
             render_pass.set_pipeline(&self.pipeline);
 
-            render_pass.set_bind_group(0, &self.atlas.diffuse_bind_group, &[]);
-            render_pass.set_bind_group(1, &uniforms, &[]);
+            render_pass.set_bind_group(0, &self.atlas.bind_group, &[]);
             render_pass.set_vertex_buffer(0, self.model.vbuf().slice(..));
+            render_pass.set_bind_group(1, &uniforms, &[]);
             //render_pass.set_vertex_buffer(1, self.instance_buffer.buff.slice(..));
             render_pass.set_index_buffer(self.model.ibuf().slice(..), wgpu::IndexFormat::Uint16);
             render_pass.draw_indexed(0..self.model.num_indices, 0, 0..1 as _);
